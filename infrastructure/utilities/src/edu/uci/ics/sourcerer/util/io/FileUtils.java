de55c1ec4c4c839a2b5e0e9255a63d2d52ea7b53
Joel Ossher <jossher@uci.edu>
2009-09-09 16:15:39 -0700

    Fixed error in license text

--- infrastructure/utilities/src/edu/uci/ics/sourcerer/util/io/FileUtils.java
+++ infrastructure/utilities/src/edu/uci/ics/sourcerer/util/io/FileUtils.java
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.package edu.uci.ics.sourcerer.util.io;
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 package edu.uci.ics.sourcerer.util.io;
 
 import static edu.uci.ics.sourcerer.util.io.Logging.logger;
