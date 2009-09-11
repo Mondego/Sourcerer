c1f2985c5851b2781b8af2e7925a0afb37aed1f8
ossher <jossher@uci.edu>
2009-09-11 12:15:54 -0700

    Fixed repository manager headers

--- infrastructure/tools/java/repository-manager/src/edu/uci/ics/sourcerer/repo/RepoJar.java
+++ infrastructure/tools/java/repository-manager/src/edu/uci/ics/sourcerer/repo/RepoJar.java
@@ -1,3 +1,20 @@
+/* 
+ * Sourcerer: an infrastructure for large-scale source code analysis.
+ * Copyright (C) by contributors. See CONTRIBUTORS.txt for full list.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package edu.uci.ics.sourcerer.repo;
 
 import static edu.uci.ics.sourcerer.util.io.Logging.logger;
@@ -9,6 +26,9 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.logging.Level;
 
+/**
+ * @author Joel Ossher (jossher@uci.edu)
+ */
 public class RepoJar {
   private long length;
   private String md5;
