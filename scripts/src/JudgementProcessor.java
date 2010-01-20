59fb15a7a58ee6357594b823215f7cf397fd33f3
Sushil Bajracharya <bajracharya@gmail.com>
2010-01-19 17:14:39 -0800

    Adding a scripts projects
    I am adding groovy scripts that I use to quickly hack things here. Also, I am using the free community edition of IntelliJ IDEA IDE for this as it has a good debugger for Groovy.

---
+++ scripts/src/JudgementProcessor.java
@@ -0,0 +1,25 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: shoeseal
+ * Date: Jan 15, 2010
+ * Time: 12:42:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JudgementProcessor {
+
+    def schemes = ['Text + FQN + sname':'s1',
+    'Text + Used FQNs/Javadoc + FQN + Jdk/Lib sim sname + sname':'s2',
+    'Text + Used FQNs/Javadoc + FQN + sname':'s3',
+    'Text + Used FQNs/Javadoc + FQN + Jdk/Lib Hamming sim sname + sname':'s4',
+    'Text + Used FQNs/Javadoc + FQN + Jdk/Lib Tanimoto sim sname + sname':'s5'
+    ]
+
+
+
+
+
+    public static void main(String[] args){
+        
+    }
+
+}
---
+++ scripts/src/TestJaws.groovy
@@ -0,0 +1,71 @@
+import edu.smu.tspell.wordnet.*;
+
+/**
+ * Displays word forms and definitions for synsets containing the word form
+ * specified on the command line. To use this application, specify the word
+ * form that you wish to view synsets for, as in the following example which
+ * displays all synsets containing the word form "airplane":
+ * <br>
+ * java TestJAWS airplane
+ */
+public class TestJaws
+{
+
+
+
+    /**
+	 * Main entry point. The command-line arguments are concatenated together
+	 * (separated by spaces) and used as the word form to look up.
+	 */
+	public static void main(String[] args)
+	{
+
+      System.setProperty("wordnet.database.dir", "/Users/shoeseal/sandbox/Sourcerer/data/wordnet_3.0-dict");
+      
+
+      if (args.length > 0)
+		{
+			//  Concatenate the command-line arguments
+			StringBuffer buffer = new StringBuffer();
+			for (int i = 0; i < args.length; i++)
+			{
+				buffer.append((i > 0 ? " " : "") + args[i]);
+			}
+			String wordForm = buffer.toString();
+			//  Get the synsets containing the wrod form
+			WordNetDatabase database = WordNetDatabase.getFileInstance();
+
+          
+			Synset[] synsets = database.getSynsets(wordForm);
+			//  Display the word forms and definitions for synsets retrieved
+			if (synsets.length > 0)
+			{
+				System.out.println("The following synsets contain '" +
+						wordForm + "' or a possible base form " +
+						"of that text:");
+				for (int i = 0; i < synsets.length; i++)
+				{
+					System.out.println("");
+					String[] wordForms = synsets[i].getWordForms();
+					for (int j = 0; j < wordForms.length; j++)
+					{
+						System.out.print((j > 0 ? ", " : "") +
+								wordForms[j]);
+					}
+					System.out.println(": " + synsets[i].getDefinition());
+				}
+			}
+			else
+			{
+				System.err.println("No synsets exist that contain " +
+						"the word form '" + wordForm + "'");
+			}
+		}
+		else
+		{
+			System.err.println("You must specify " +
+					"a word form for which to retrieve synsets.");
+		}
+	}
+
+}
