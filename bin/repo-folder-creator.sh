586ffd01954da258edb6e6d41f98c00972fc64f3
Sushil Bajracharya <bajracharya@gmail.com>
2010-04-07 02:44:17 -0700

    shell scripts to run individual tools

---
+++ bin/repo-folder-creator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# =====================================================================
+# Sourcerer: An infrastructure for large-scale source code analysis.
+# Copyright (C) by contributors. See CONTRIBUTORS.txt for full list.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# ===================================================================== 
+# @author Sushil Bajracharya (bajracharya@gmail.com)
+
+L="../lib" # library path
+LIB_INCLUDES=$L/ant-1.7.0.jar:$L/ant-commons-net-1.7.0.jar:$L/ant-launcher-1.7.0.jar:$L/commons-cli-1.1.jar:$L/guice-1.0.jar:$L/svnclientadapter-svnant_1.2.1.jar
+RM_JAR="core-repomanager.jar" # core repomanager jar
+
+java -cp ./:$LIB_INCLUDES:./$RM_JAR edu.uci.ics.sourcerer.repomanager.RepoFolderCreatorRunner $1 $2 $3 $4 $5 $6 $7 $8 $9 "${10}" "${11}" "${12}"
---
+++ bin/run-raw-usage-writer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# =====================================================================
+# Sourcerer: An infrastructure for large-scale source code analysis.
+# Copyright (C) by contributors. See CONTRIBUTORS.txt for full list.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# ===================================================================== 
+# @author Sushil Bajracharya (bajracharya@gmail.com)
+
+L="../lib" # library path
+LIB_INCLUDES=$L/mahout-core-0.3-SNAPSHOT.jar:$L/slf4j-api-1.5.8.jar:$L/slf4j-jcl-1.5.8.jar:$L/commons-logging-1.1.1.jar:$L/uncommons-maths-1.2.jar:$L/mysql-connector-java-5.1.7-bin.jar:$L/commons-dbcp-1.2.2.jar:$L/commons-pool-1.5.4.jar:$L/j2ee.jar
+SW_JAR="sourcerer-ml.jar"
+
+java -Xmx6048m -cp ./:$LIB_INCLUDES:./sourcerer-db.jar:./$SW_JAR edu.uci.ics.sourcerer.ml.db.tools.UsagePreCalculatorRunner $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}
+
+# nohup ./run-raw-usage-writer.sh --exclude-internal false  --output /data/sourcerer/ml-data/method-usage-all/ --database-url jdbc:mysql://tagus.ics.uci.edu:3306/sourcerer_eclipse --database-user <user> --database-password <pass> --entity-type METHOD > rawusage-mall.out &
+
