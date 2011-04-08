88371ed50bfc7847ccbd4e03fdda0518d7b00e24
ossher <jossher@ics.uci.edu>
2011-04-08 13:30:42 -0700

    Updated repository statistics tools

--- infrastructure/tools/java/repository-manager/src/edu/uci/ics/sourcerer/repo/stats/RepositoryStatistics.java
+++ infrastructure/tools/java/repository-manager/src/edu/uci/ics/sourcerer/repo/stats/RepositoryStatistics.java
@@ -17,9 +17,22 @@
  */
 package edu.uci.ics.sourcerer.repo.stats;
 
+import static edu.uci.ics.sourcerer.util.io.Logging.logger;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import edu.uci.ics.sourcerer.repo.base.IFileSet;
+import edu.uci.ics.sourcerer.repo.base.RepoProject;
 import edu.uci.ics.sourcerer.repo.base.Repository;
+import edu.uci.ics.sourcerer.repo.extracted.ExtractedProject;
 import edu.uci.ics.sourcerer.repo.extracted.ExtractedRepository;
+import edu.uci.ics.sourcerer.repo.general.IndexedJar;
+import edu.uci.ics.sourcerer.repo.general.JarIndex;
+import edu.uci.ics.sourcerer.util.io.FileUtils;
 import edu.uci.ics.sourcerer.util.io.Property;
+import edu.uci.ics.sourcerer.util.io.TablePrettyPrinter;
 import edu.uci.ics.sourcerer.util.io.properties.StringProperty;
 
 /**
@@ -27,12 +40,98 @@ import edu.uci.ics.sourcerer.util.io.properties.StringProperty;
  */
 public class RepositoryStatistics {
   public static final Property<String> JAR_STATS_FILE = new StringProperty("jar-stats-file", "jar-stats.txt", "File containing repository jar statistics.");
-  public static void printJarStatistics(Repository repo) {}
+  public static void printJarStatistics(Repository repo) {
+    JarIndex index = repo.getJarIndex();
+    int project = 0;
+    int projectSource = 0;
+    int maven = 0;
+    int mavenSource = 0;
+    for (IndexedJar jar : index.getJars()) {
+      if (jar.isMavenJar()) {
+        maven++;
+        if (jar.hasSource()) {
+          mavenSource++;
+        }
+      } else {
+        project++;;
+        if (jar.hasSource()) {
+          projectSource++;
+        }
+      }
+    }
+      
+    TablePrettyPrinter printer = null;
+    try {
+      printer = TablePrettyPrinter.getTablePrettyPrinter(JAR_STATS_FILE);
+      printer.beginTable(2);
+      printer.addDividerRow();
+      printer.beginRow();
+      printer.addCell("Jars from Projects");
+      printer.addCell(project);
+      printer.beginRow();
+      printer.addCell("Jars from Projects with Source");
+      printer.addCell(projectSource);
+      printer.beginRow();
+      printer.addCell("Maven Jars");
+      printer.addCell(maven);
+      printer.beginRow();
+      printer.addCell("Maven Jars with Source");
+      printer.addCell(mavenSource);
+    } finally {
+      printer.close();
+    }
+  }
   
   public static final Property<String> PROJECT_SIZES_FILE = new StringProperty("project-sizes-file", "project-sizes.txt", "File containing project size information");
-  public static void printProjectSizes(Repository repo) {}
+  public static void printProjectSizes(Repository repo) {
+    BufferedWriter bw = null;
+    try {
+      bw = FileUtils.getBufferedWriter(PROJECT_SIZES_FILE);
+      bw.write("project-path total-file-count filtered-file-count jar-count");
+      bw.newLine();
+      for (RepoProject project : repo.getProjects()) {
+        IFileSet files = project.getFileSet();
+        bw.write(project.getProjectRoot().getRelativePath() + " " + files.getJavaFileCount() + " " + files.getFilteredJavaFileCount() + " " + files.getJarFileCount());
+        bw.newLine();
+      }
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Error writing to file", e);
+    } finally { 
+      FileUtils.close(bw);
+    }
+  }
   
   public static final Property<String> PROJECT_NAMES_FILE = new StringProperty("project-names-file", "project-names.txt", "File containing the names of the projects in the repository.");
-  public static void printProjectNames(Repository repo) {}
-  public static void printProjectNames(ExtractedRepository repo) {}
+  public static void printProjectNames(Repository repo) {
+    BufferedWriter bw = null;
+    try {
+      bw = FileUtils.getBufferedWriter(PROJECT_NAMES_FILE);
+      bw.write("project-path project-name");
+      bw.newLine();
+      for (RepoProject project : repo.getProjects()) {
+        bw.write(project.getProjectRoot().getRelativePath() + " " + project.loadProperties().getName());
+        bw.newLine();
+      }
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Error writing to file", e);
+    } finally { 
+      FileUtils.close(bw);
+    }
+  }
+  public static void printProjectNames(ExtractedRepository repo) {
+    BufferedWriter bw = null;
+    try {
+      bw = FileUtils.getBufferedWriter(PROJECT_NAMES_FILE);
+      bw.write("project-path project-name");
+      bw.newLine();
+      for (ExtractedProject project : repo.getProjects()) {
+        bw.write(project.getRelativePath() + " " + project.getName());
+        bw.newLine();
+      }
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Error writing to file", e);
+    } finally { 
+      FileUtils.close(bw);
+    }
+  }
 }
