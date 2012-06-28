80f3437675f677b2cfdbcc0ef2e76849f854cfee
ossher <jossher@uci.edu>
2012-06-27 22:40:39 -0700

    Another minor duplicate fix

--- infrastructure/tools/java/database-importer/src/edu/uci/ics/sourcerer/tools/java/db/importer/resolver/ModeledEntity.java
+++ infrastructure/tools/java/database-importer/src/edu/uci/ics/sourcerer/tools/java/db/importer/resolver/ModeledEntity.java
@@ -81,7 +81,7 @@ public class ModeledEntity {
           exec.insert(RelationsTable.makeInsert(Relation.MATCHES, RelationClass.EXTERNAL, entityID, dupID, projectID));
         }
       } else if (virtualDuplicates != null) {
-        entityID = exec.insertWithKey(EntitiesTable.makeInsert(Entity.VIRTUAL_DUPLICATE, fqn, projectID));
+        entityID = exec.insertWithKey(EntitiesTable.makeInsert(Entity.VIRTUAL_DUPLICATE, null, projectID));
         if (virtualDuplicates.size() <= 1) {
           logger.severe("Single virtual duplicate! " + virtualDuplicates.toString());
         }
